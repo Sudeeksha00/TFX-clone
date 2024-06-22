c6c1a2b8235abeea12f75648181312e3229c337c
Sudeeksha00 <135346627+Sudeeksha00@users.noreply.github.com>
2024-06-22 17:45:15 +0530

    added descriptive comments

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -7,14 +7,20 @@ from configurations.config import Config
 
 def download_dataset(LOCAL_FILE_NAME, url):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    # disabing warnings associated with potential security risks due to insecure connections. (not recommended for production use) 
     c = urllib3.PoolManager()
+    # creating a connection pool to manage HTTP requests efficently.
     with c.request("GET", url, preload_content=False) as res, open(
         LOCAL_FILE_NAME, "wb"
     ) as out_file:
         shutil.copyfileobj(res, out_file)
     logging.info("Download completed.")
+# GET request to specified URL and opens the file to write in binary mode. (wb)
+# preload_content = FALSE: don't load entire content into memory at once. This will be useful for large datasets.
+# shutil.... : copies the data into a local output file
 
-
+# This function determines the root folder of the project.
+# ARDHAMKAALE final ga em "path" return chestundhi ani...?
 def get_folder():
     path = os.getcwd()
     while True:
@@ -26,7 +32,8 @@ def get_folder():
             raise Exception('Check Wheather project name set correctly')
     return path
 
-
+# It checks whether current code is run as main script or if its being imported as a module from another script.
+# This makes sure that this code is executed only when it is run as main script.
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
